b73ecc059327e6cb2fa0b2c7052bcf1e2f2fa99c
LuoliKo <widung@163.com>
2018-06-20 11:43:10 +0800

    project create

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'elecmark'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
